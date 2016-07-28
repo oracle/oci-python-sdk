a29c6f0e178ee6f9938c50b12f1ab9f3be3e217f
Brian Gustafson <brian.gustafson@oracle.com>
2016-07-28 08:21:30 -0700

    Respond to comments

--- oraclebmi/api_client.py
+++ oraclebmi/api_client.py
@@ -125,6 +125,7 @@ class ApiClient(object):
 
         if body:
             body = self.sanitize_for_serialization(body)
+            body = json.dumps(body)
 
         url = endpoint + resource_path
 
@@ -133,7 +134,7 @@ class ApiClient(object):
                                  auth=self.signer,
                                  params=query_params,
                                  headers=header_params,
-                                 data=json.dumps(body))
+                                 data=body)
 
         is_error = response.status_code not in range(200, 299)
         if is_error:
@@ -182,8 +183,7 @@ class ApiClient(object):
         :return: The serialized form of data.
         """
         types = (str, int, float, bool, tuple)
-        if sys.version_info < (3,0):
-            types = types + (unicode,)
+
         if isinstance(obj, type(None)):
             return None
         elif isinstance(obj, types):
@@ -217,7 +217,7 @@ class ApiClient(object):
 
         # In the python 3, the response.data is bytes.
         # we need to decode it to string.
-        if response_data and sys.version_info > (3,):
+        if response_data:
             response_data = response_data.decode('utf8')
 
         try:
