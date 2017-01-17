7bc635646ed60723b59ab807074ca1eb0094ae3e
Joe Cross <joe.cross@oracle.com>
2017-01-17 11:42:47 -0800

    Add runnable examples

---
+++ examples/pagination.py
@@ -0,0 +1,34 @@
+import oraclebmc
+
+
+config = oraclebmc.config.from_file()
+compartment_id = config["tenancy"]
+identity = oraclebmc.identity.IdentityClient(config)
+
+
+# operation-independent pagination function
+def paginate(operation, *args, **kwargs):
+    """Yields values from a list call, automatically following pagination tokens.
+
+    Metadata such as request_id, headers, and http status are not returned.
+    :param operation: a client list function such as identity.list_policies
+    """
+    while True:
+        response = operation(*args, **kwargs)
+        for value in response.data:
+            yield value
+        kwargs["page"] = response.next_page
+        if not response.has_next_page:
+            break
+
+
+for user in paginate(
+        identity.list_users,
+        compartment_id=compartment_id):
+    print("User: " + str(user))
+
+
+for group in paginate(
+        identity.list_groups,
+        compartment_id=compartment_id):
+    print("Group: " + str(group))
---
+++ examples/user_crud.py
@@ -0,0 +1,52 @@
+import oraclebmc
+from oraclebmc.identity.models import AddUserToGroupDetails, CreateGroupDetails, CreateUserDetails
+
+# Default config file and profile
+config = oraclebmc.config.from_file()
+compartment_id = config["tenancy"]
+# Service client
+identity = oraclebmc.identity.IdentityClient(config)
+
+
+user_name = "python-sdk-example-user"
+group_name = "python-sdk-example-group"
+
+
+print("Creating a new user {!r} in compartment {!r}".format(
+    user_name, compartment_id))
+
+request = CreateUserDetails()
+request.compartment_id = compartment_id
+request.name = user_name
+request.description = "Created by a Python SDK example"
+user = identity.create_user(request)
+print(user.data)
+
+
+print("Creating a new group {!r} in compartment {!r}".format(
+    group_name, compartment_id))
+
+request = CreateGroupDetails()
+request.compartment_id = compartment_id
+request.name = group_name
+request.description = "Created by a Python SDK example"
+group = identity.create_group(request)
+print(group.data)
+
+print("Adding new user to the new group")
+request = AddUserToGroupDetails()
+request.user_id = user.data.id
+request.group_id = group.data.id
+membership = identity.add_user_to_group(request)
+print(membership.data)
+
+print("Cleaning up resources.")
+print("Removing {!r} from {!r}.".format(
+    user_name, group_name))
+identity.remove_user_from_group(membership.data.id)
+
+print("Deleting user {!r}".format(user_name))
+identity.delete_user(user.data.id)
+
+print("Deleting group {!r}".format(group_name))
+identity.delete_group(group.data.id)
