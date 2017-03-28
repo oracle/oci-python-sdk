c72c6709f5eb57feffa50084c5b5b3133bbd0cde
Brian Gustafson <brian.gustafson@oracle.com>
2017-03-28 13:23:02 -0700

    Fix a few more errors in Team City
    Found a few more places where our tests rely on files outside of the
    repo. Also, removing the python version from tox.ini - this is what
    we have for the CLI, where is seems to be working just fine.

--- tests/unit/test_errors.py
+++ tests/unit/test_errors.py
@@ -22,13 +22,8 @@ def test_invalid_policy(identity, config):
     tests.util.validate_service_error(excinfo.value, 400, "InvalidParameter", "The group/compartment specified")
 
 
-def test_invalid_endpoint_host():
-    config = oraclebmc.config.from_file(
-        file_location=tests.util.get_resource_path('config'),
-        profile_name="DEFAULT"
-    )
-    client = oraclebmc.identity.IdentityClient(config)
-    client.base_client.endpoint = "https://identity.us-phoenix-999999.oraclecloud.com/v1"
+def test_invalid_endpoint_host(identity):
+    identity.base_client.endpoint = "https://identity.us-phoenix-999999.oraclecloud.com/v1"
 
     with pytest.raises(Exception):
-        client.list_users('invalid_compartment')
+        identity.list_users('invalid_compartment')
