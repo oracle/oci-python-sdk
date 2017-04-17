6cb2f9021fac4142bb5fe9f38674ed67529b172f
Art Hall <art.hall.iii@oracle.com>
2017-04-17 12:46:37 -0400

    Example code to show how to do multiprocess uploads to Object Storage.
        Use context manager to open file
        Fix grammer issues

--- examples/parallel_upload_to_object_storage.py
+++ examples/parallel_upload_to_object_storage.py
@@ -4,7 +4,7 @@
 # using multiple processes so that the uploads are done in parallel.
 #
 # Assumptions: Object storage bucket already exists. See object_crud.py for
-# example of creating a bucket.
+# an example of creating a bucket.
 
 import oraclebmc
 import os
@@ -15,7 +15,7 @@ from glob import glob
 
 def upload_to_object_storage(config, namespace, bucket, path):
     """
-    upload_to_object_storage will upload a file to a object storage bucket.
+    upload_to_object_storage will upload a file to an object storage bucket.
     This function is intended to be run as a separate process.  The client is
     created with each invocation so that the separate processes do
     not have a reference to the same client.
@@ -26,15 +26,14 @@ def upload_to_object_storage(config, namespace, bucket, path):
     :param path: path to file to upload to object storage
     :rtype: None
     """
-    in_file = open(path, "rb")
-    data = in_file.read()
-    name = os.path.basename(path)
-    ostorage = oraclebmc.object_storage.ObjectStorageClient(config)
-    ostorage.put_object(namespace,
+    with open(path, "rb") as in_file:
+        name = os.path.basename(path)
+        ostorage = oraclebmc.object_storage.ObjectStorageClient(config)
+        ostorage.put_object(namespace,
                         bucket,
                         name,
-                        data)
-    print("Finished uploading {}".format(name))
+                        in_file)
+        print("Finished uploading {}".format(name))
 
 if __name__ == "__main__":
     config = oraclebmc.config.from_file()
