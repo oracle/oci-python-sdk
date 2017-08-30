28a3db524ea552bcbee8d0b35019d6a7c0a169e2
Mike Ross <mike.c.ross@oracle.com>
2017-08-30 13:05:16 -0700

    Updating raw requests example to match other examples and not hardcode config locaiton

--- examples/raw_request.py
+++ examples/raw_request.py
@@ -4,7 +4,7 @@ import requests
 from oci.config import from_file
 from oci.signer import Signer
 
-config = from_file('~/.oci/config')
+config = from_file()
 auth = Signer(
     tenancy=config['tenancy'],
     user=config['user'],
