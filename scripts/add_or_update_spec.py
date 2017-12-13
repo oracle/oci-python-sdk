76c55ebdb0e91de959779d18a7c112a4164bacb4
Mike Ross <mike.c.ross@oracle.com>
2017-12-13 14:16:27 -0800

    Pretty print pom and expose params on add_or_update_spec script through click

--- scripts/add_or_update_spec.py
+++ scripts/add_or_update_spec.py
@@ -1,57 +1,63 @@
 #!/usr/bin/env python
 
+#
+# This script manipulates pom.xml to either add new specs or update the versions of existing specs.
+#
+
 import xml.etree.ElementTree as ET
 import re
+import click
+from click.exceptions import UsageError
 
 POM_LOCATION = "pom.xml"
 
 INITIAL_DEPENDENCY_TEMPLATE = """
-        <dependency>
-            <groupId>{group_id}</groupId>
-            <artifactId>{artifact_id}</artifactId>
-        </dependency>
+<dependency>
+    <groupId>{group_id}</groupId>
+    <artifactId>{artifact_id}</artifactId>
+</dependency>
 """
 
 UNPACK_EXECUTION_TEMPLATE = """
-    <execution>
-        <id>unpack-{spec_name}</id>
-        <phase>initialize</phase>
-        <goals>
-            <goal>unpack</goal>
-        </goals>
-        <configuration>
-            <artifactItems>
-                <artifactItem>
-                    <groupId>{group_id}</groupId>
-                    <artifactId>{artifact_id}</artifactId>
-                    <type>jar</type>
-                    <includes>{spec_path_relative_to_jar}</includes>
-                    <outputDirectory>${{spec-temp-dir}}/{spec_name}</outputDirectory>
-                </artifactItem>
-            </artifactItems>
-        </configuration>
-    </execution>
+<execution>
+    <id>unpack-{spec_name}</id>
+    <phase>initialize</phase>
+    <goals>
+        <goal>unpack</goal>
+    </goals>
+    <configuration>
+        <artifactItems>
+            <artifactItem>
+                <groupId>{group_id}</groupId>
+                <artifactId>{artifact_id}</artifactId>
+                <type>jar</type>
+                <includes>{spec_path_relative_to_jar}</includes>
+                <outputDirectory>${{spec-temp-dir}}/{spec_name}</outputDirectory>
+            </artifactItem>
+        </artifactItems>
+    </configuration>
+</execution>
 """
 
 GENERATE_EXECUTION_TEMPLATE = """
-    <execution>
-        <id>python-public-sdk-{spec_name}</id>
-        <phase>compile</phase>
-        <goals>
-            <goal>generate</goal>
-        </goals>
-        <configuration>
-            <language>oracle-python-sdk</language>
-            <specPath>${{spec-temp-dir}}/{spec_name}/{spec_path_relative_to_jar}</specPath>
-            <outputDir>src/oci</outputDir>
-            <basePackage>OCI</basePackage>
-            <additionalProperties>
-                <specName>{spec_name}</specName>
-                <generateInitFile>true</generateInitFile>
-                <endpoint>{endpoint}</endpoint>
-            </additionalProperties>
-        </configuration>
-    </execution>
+<execution>
+    <id>python-public-sdk-{spec_name}</id>
+    <phase>compile</phase>
+    <goals>
+        <goal>generate</goal>
+    </goals>
+    <configuration>
+        <language>oracle-python-sdk</language>
+        <specPath>${{spec-temp-dir}}/{spec_name}/{spec_path_relative_to_jar}</specPath>
+        <outputDir>src/oci</outputDir>
+        <basePackage>OCI</basePackage>
+        <additionalProperties>
+            <specName>{spec_name}</specName>
+            <generateInitFile>true</generateInitFile>
+            <endpoint>{endpoint}</endpoint>
+        </additionalProperties>
+    </configuration>
+</execution>
 """
 
 CLEAN_ELEMENT_TEMPLATE = """
@@ -149,30 +155,64 @@ def update_version_of_existing_spec(pom, artifact_id, version):
     dependency.find('./ns:version', ns).text = version
 
 
-# params that we will ask user for
-group_id = "com.oracle.pic.orchestration"
-artifact_id = "maestro-spec"
-version = "0.0.1-SNAPSHOT"
-spec_name = "orchestration"
-spec_path_relative_to_jar = "api.yaml"
-endpoint = "https://orchestration.{domain}/20170630"
+def indent(elem, level=0):
+    indent_str = "    "
+    i = "\n" + level*indent_str
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + indent_str
+        for e in elem:
+            indent(e, level+1)
+            if not e.tail or not e.tail.strip():
+                e.tail = i + indent_str
+        if not e.tail or not e.tail.strip():
+            e.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
 
-pom = parse_pom()
 
-# determine if this artifact is already in the spec
-xpath_for_spec_dependency_declaration = ".//ns:dependency[ns:artifactId='{artifact_id}']".format(artifact_id=artifact_id)
-if (pom.findall(xpath_for_spec_dependency_declaration, ns)):
-    print('Artifact {} already exists in pom.xml. Updating version...'.format(spec_name))
-    update_version_of_existing_spec(pom, artifact_id, version)
-else:
-    print('Artifact {} does not exist in pom.xml. Adding it...'.format(spec_name))
-    generate_and_add_unpack_element(pom, spec_name, group_id, artifact_id, spec_path_relative_to_jar)
-    generate_and_add_generate_section(pom, spec_name, spec_path_relative_to_jar, endpoint)
-    generate_and_add_clean_section(pom, spec_name)
-    generate_and_add_dependency_management_section(pom, group_id, artifact_id, version)
-    generate_and_add_initial_dependency_section(pom, group_id, artifact_id)
+@click.command()
+@click.option('--artifact-id', required=True, help='The artifact id for the spec artifact (e.g. coreservices-api-spec')
+@click.option('--group-id', help='The group id for the spec artifact (e.g. com.oracle.pic.commons)')
+@click.option('--spec-name', help='The name of the spec. This will be used (e.g. core, identity, object_storage). This is also used as the module name.')
+@click.option('--relative-spec-path', help='The relative path of the spec within the artifact (e.g. coreservices-api-spec-20160918-external.yaml)')
+@click.option('--endpoint', help='The base endpoint for the service (e.g. https://iaas.{domain}/20160918)')
+@click.option('--version', required=True, help='The version of the spec artifact (e.g. 0.0.1-SNAPSHOT')
+def add_or_update_spec(artifact_id, group_id, spec_name, relative_spec_path, endpoint, version):
+    pom = parse_pom()
 
-# write out pom
-pom.write(POM_LOCATION, xml_declaration=True, encoding='UTF-8')
+    # determine if this artifact is already in the spec
+    xpath_for_spec_dependency_declaration = ".//ns:dependency[ns:artifactId='{artifact_id}']".format(artifact_id=artifact_id)
+    if (pom.findall(xpath_for_spec_dependency_declaration, ns)):
+        print('Artifact {} already exists in pom.xml. Updating version...'.format(artifact_id))
+        update_version_of_existing_spec(pom, artifact_id, version)
+    else:
+        if not group_id:
+            raise UsageError('Must specify --group-id for new spec')
 
-print('Success!')
+        if not spec_name:
+            raise UsageError('Must specify --spec-name for new spec')
+
+        if not relative_spec_path:
+            raise UsageError('Must specify --relative-spec-path for new spec')
+
+        if not endpoint:
+            raise UsageError('Must specify --endpoint for new spec')
+
+        print('Artifact {} does not exist in pom.xml. Adding it...'.format(spec_name))
+        generate_and_add_unpack_element(pom, spec_name, group_id, artifact_id, relative_spec_path)
+        generate_and_add_generate_section(pom, spec_name, relative_spec_path, endpoint)
+        generate_and_add_clean_section(pom, spec_name)
+        generate_and_add_dependency_management_section(pom, group_id, artifact_id, version)
+        generate_and_add_initial_dependency_section(pom, group_id, artifact_id)
+
+    # pretty print pom
+    indent(pom.getroot())
+    pom.write(POM_LOCATION, encoding="UTF-8", xml_declaration=True) 
+
+    print('Success!')
+
+
+if __name__ == '__main__':
+    add_or_update_spec()
