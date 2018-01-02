dee272b5ebc04c0a08644b00443625ea2fa165dd
Nathan Vu <nathan.vu@oracle.com>
2018-01-02 17:43:21 +1100

    Add comments, and add method variants that can produce generators (for lazily loading results)

--- tests/unit/test_paging.py
+++ tests/unit/test_paging.py
@@ -4,6 +4,7 @@ from requests.exceptions import ConnectionError
 import oci
 import oci.pagination
 import oci.pagination.internal
+import os
 import pytest
 
 PAGE_SIZE = 10
@@ -49,13 +50,132 @@ def test_pagination_get_all_results(identity, config):
 
 def test_pagination_get_up_to_limit(identity, config):
     # There may not be 70 users, so only check that we get no more than what we ask for
-    list_users_response = oci.pagination.list_call_get_up_to_limit(identity.list_users, 10, 70, config["tenancy"])
+    list_users_response = oci.pagination.list_call_get_up_to_limit(identity.list_users, 70, 10, config["tenancy"])
     assert len(list_users_response.data) <= 70
 
     # There should always be at least one user, so asking for one should get us exactly one
     list_users_response = oci.pagination.list_call_get_up_to_limit(identity.list_users, 1, 1, config["tenancy"])
     assert list_users_response.has_next_page
     assert len(list_users_response.data) == 1
+
+
+def test_pagination_get_up_to_limit_none_limit(identity, config):
+    list_users_response = oci.pagination.list_call_get_up_to_limit(identity.list_users, None, 10, config["tenancy"])
+    assert list_users_response.has_next_page
+    assert len(list_users_response.data) <= 10
+
+
+def test_pagination_get_all_yields(identity, config):
+    all_users = []
+    found_test_user = True
+    for user in oci.pagination.list_call_get_all_results_generator(identity.list_users, 'record', config["tenancy"]):
+        assert user.id is not None
+        assert user.name is not None
+        all_users.append(user)
+        if config["user"] == user.id:
+            found_test_user = True
+
+    assert len(all_users) > 1
+    assert found_test_user
+
+    all_users_response = oci.pagination.list_call_get_all_results(identity.list_users, config["tenancy"])
+
+    # Some jitter in case other tests are adding/removing users, but they should be close to each other
+    assert abs(len(all_users_response.data) - len(all_users)) < 5
+
+
+def test_pagination_get_up_to_limit_yields_none_limit(identity, config):
+    num_iterations = 0
+    call_result = None
+    for response in oci.pagination.list_call_get_up_to_limit_generator(identity.list_users, None, 10, 'response', config["tenancy"]):
+        call_result = response
+        num_iterations += 1
+
+    assert num_iterations == 1  # Since we had a none limit, yield a single page
+    assert len(call_result.data) > 1
+    assert len(call_result.data) <= 10
+
+    users = []
+    num_iterations = 0
+    for user in oci.pagination.list_call_get_up_to_limit_generator(identity.list_users, None, 10, 'record', config["tenancy"]):
+        assert user.id is not None
+        assert user.name is not None
+        num_iterations += 1
+        users.append(user)
+    assert num_iterations == len(users)
+    assert len(users) == len(call_result.data)
+
+
+def test_pagination_get_up_to_limit_yields(identity, config):
+    num_iterations = 0
+    users_from_response_gen = []
+    for response in oci.pagination.list_call_get_up_to_limit_generator(identity.list_users, 70, 10, 'response', config["tenancy"]):
+        num_iterations += 1
+        users_from_response_gen.extend(response.data)
+    assert num_iterations > 1
+    assert num_iterations <= 7
+    assert len(users_from_response_gen) <= 70
+
+    users = []
+    num_iterations = 0
+    for user in oci.pagination.list_call_get_up_to_limit_generator(identity.list_users, 70, 10, 'record', config["tenancy"]):
+        assert user.id is not None
+        assert user.name is not None
+        num_iterations += 1
+        users.append(user)
+    assert num_iterations > 1
+    assert num_iterations <= 70
+    assert len(users_from_response_gen) == len(users)
+
+    users = []
+    num_iterations = 0
+    for user in oci.pagination.list_call_get_up_to_limit_generator(identity.list_users, 1, 1, 'record', config["tenancy"]):
+        assert user.id is not None
+        assert user.name is not None
+        num_iterations += 1
+        users.append(user)
+    assert len(users) == 1
+    assert num_iterations == 1
+
+
+def test_pagination_when_no_data_exists(compute):
+    all_records_eager = oci.pagination.list_call_get_all_results(compute.list_images, os.environ.get("OCI_PYSDK_COMPARTMENT_ID"), operating_system='superman fake os')
+    limit_records_eager = oci.pagination.list_call_get_up_to_limit(compute.list_images, 20, 5, os.environ.get("OCI_PYSDK_COMPARTMENT_ID"), operating_system='superman fake os')
+
+    assert len(all_records_eager.data) == 0
+    assert len(limit_records_eager.data) == 0
+
+    num_iterations = 0
+    call_result = None
+    for response in oci.pagination.list_call_get_all_results_generator(compute.list_images, 'response', os.environ.get("OCI_PYSDK_COMPARTMENT_ID"), operating_system='superman fake os'):
+        num_iterations += 1
+        call_result = response
+    assert num_iterations == 1
+    assert len(call_result.data) == 0
+
+    num_iterations = 0
+    call_result = None
+    for response in oci.pagination.list_call_get_up_to_limit_generator(compute.list_images, 20, 5, 'response', os.environ.get("OCI_PYSDK_COMPARTMENT_ID"), operating_system='superman fake os'):
+        num_iterations += 1
+        call_result = response
+    assert num_iterations == 1
+    assert len(call_result.data) == 0
+
+    num_iterations = 0
+    records = []
+    for image in oci.pagination.list_call_get_all_results_generator(compute.list_images, 'record', os.environ.get("OCI_PYSDK_COMPARTMENT_ID"), operating_system='superman fake os'):
+        num_iterations += 1
+        records.append(image)
+    assert num_iterations == 0
+    assert len(records) == 0
+
+    num_iterations = 0
+    records = []
+    for image in oci.pagination.list_call_get_up_to_limit_generator(compute.list_images, 20, 5, 'record', os.environ.get("OCI_PYSDK_COMPARTMENT_ID"), operating_system='superman fake os'):
+        num_iterations += 1
+        records.append(image)
+    assert num_iterations == 0
+    assert len(records) == 0
 
 
 def test_pagination_internal_limit_retry_checker():
