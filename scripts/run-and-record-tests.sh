956d11a1e83f212543c0b1332d8a19d53c4de2bf
Nathan Vu <nathan.vu@oracle.com>
2018-01-24 15:51:37 -0800

    Add script so that we can run/re-record VCR responses in TeamCity

---
+++ scripts/run-and-record-tests.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# This script is used to: 
+#   - Run the tests without using VCR to mock responses (by deleting the old mock responses)
+#   - Re-record the tests so that we keep our VCR data up to date
+
+set -e ;
+set -x ;
+
+echo Check Directory Contents
+ls -la
+
+echo "Sourcing test_setup.sh to load env vars"
+source internal_resources/test_setup.sh;
+
+echo Creating venv to install sdk locally
+. /opt/odo/tox_sic/venv/bin/activate
+virtualenv .sdk-venv
+. .sdk-venv/bin/activate
+pip install -e .
+
+echo Python version
+python --version
+
+echo Running Tests
+
+if [ $TEST_ENABLE = "false" ]; then
+  echo "TESTS HAVE BEEN DISABLED."
+else
+  pip install tox
+
+  echo "Removing mocked responses before Python 2.7 test run"
+  rm tests/fictures/cassettes/*.yml
+
+  echo "Running Python 2.7 tests"
+  tox -e py27 -- --vcr-record-mode=once
+
+  echo "Removing mocked responses before Python 3.5 test run"
+  rm tests/fictures/cassettes/*.yml
+
+  echo "Running Python 3.5 tests"
+  tox -e py35 -- --vcr-record-mode=once
+fi
