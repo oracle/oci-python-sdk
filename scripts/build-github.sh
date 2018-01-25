2eb69e62584d22d9c681c740680198eb7f999da0
Nathan Vu <nathan.vu@oracle.com>
2018-01-25 14:53:15 -0800

    Fixup GitHub build script to install vcr dependency

--- scripts/build-github.sh
+++ scripts/build-github.sh
@@ -65,6 +65,7 @@ else
   # *github* version of the SDK (which was installed above)
   pip install pytest
   pip install mock
+  pip install vcr
 
   # run tests from bitbucket repository because not all tests are copied into github
   cd .python-sdk-bitbucket/
