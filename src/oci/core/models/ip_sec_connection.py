0c00d52d2b454e04e4f16677f3220ea6a1f813fb
Art Hall <art.hall.iii@oracle.com>
2018-05-15 16:30:15 -0400

    UDX-919 and UDX-920
    Update featureID.yaml to whitelist udx-919 and udx-920
    Update release-sdk.yaml to remove the blacklisting of the VNC tagging docs

--- src/oci/core/models/ip_sec_connection.py
+++ src/oci/core/models/ip_sec_connection.py
@@ -51,6 +51,10 @@ class IPSecConnection(object):
             The value to assign to the cpe_id property of this IPSecConnection.
         :type cpe_id: str
 
+        :param defined_tags:
+            The value to assign to the defined_tags property of this IPSecConnection.
+        :type defined_tags: dict(str, dict(str, object))
+
         :param display_name:
             The value to assign to the display_name property of this IPSecConnection.
         :type display_name: str
@@ -58,6 +62,10 @@ class IPSecConnection(object):
         :param drg_id:
             The value to assign to the drg_id property of this IPSecConnection.
         :type drg_id: str
+
+        :param freeform_tags:
+            The value to assign to the freeform_tags property of this IPSecConnection.
+        :type freeform_tags: dict(str, str)
 
         :param id:
             The value to assign to the id property of this IPSecConnection.
@@ -81,8 +89,10 @@ class IPSecConnection(object):
         self.swagger_types = {
             'compartment_id': 'str',
             'cpe_id': 'str',
+            'defined_tags': 'dict(str, dict(str, object))',
             'display_name': 'str',
             'drg_id': 'str',
+            'freeform_tags': 'dict(str, str)',
             'id': 'str',
             'lifecycle_state': 'str',
             'static_routes': 'list[str]',
@@ -92,8 +102,10 @@ class IPSecConnection(object):
         self.attribute_map = {
             'compartment_id': 'compartmentId',
             'cpe_id': 'cpeId',
+            'defined_tags': 'definedTags',
             'display_name': 'displayName',
             'drg_id': 'drgId',
+            'freeform_tags': 'freeformTags',
             'id': 'id',
             'lifecycle_state': 'lifecycleState',
             'static_routes': 'staticRoutes',
@@ -102,8 +114,10 @@ class IPSecConnection(object):
 
         self._compartment_id = None
         self._cpe_id = None
+        self._defined_tags = None
         self._display_name = None
         self._drg_id = None
+        self._freeform_tags = None
         self._id = None
         self._lifecycle_state = None
         self._static_routes = None
@@ -158,6 +172,40 @@ class IPSecConnection(object):
         self._cpe_id = cpe_id
 
     @property
+    def defined_tags(self):
+        """
+        Gets the defined_tags of this IPSecConnection.
+        Defined tags for this resource. Each key is predefined and scoped to a namespace.
+        For more information, see `Resource Tags`__.
+
+        Example: `{\"Operations\": {\"CostCenter\": \"42\"}}`
+
+        __ https://docs.us-phoenix-1.oraclecloud.com/Content/General/Concepts/resourcetags.htm
+
+
+        :return: The defined_tags of this IPSecConnection.
+        :rtype: dict(str, dict(str, object))
+        """
+        return self._defined_tags
+
+    @defined_tags.setter
+    def defined_tags(self, defined_tags):
+        """
+        Sets the defined_tags of this IPSecConnection.
+        Defined tags for this resource. Each key is predefined and scoped to a namespace.
+        For more information, see `Resource Tags`__.
+
+        Example: `{\"Operations\": {\"CostCenter\": \"42\"}}`
+
+        __ https://docs.us-phoenix-1.oraclecloud.com/Content/General/Concepts/resourcetags.htm
+
+
+        :param defined_tags: The defined_tags of this IPSecConnection.
+        :type: dict(str, dict(str, object))
+        """
+        self._defined_tags = defined_tags
+
+    @property
     def display_name(self):
         """
         Gets the display_name of this IPSecConnection.
@@ -206,6 +254,42 @@ class IPSecConnection(object):
         :type: str
         """
         self._drg_id = drg_id
+
+    @property
+    def freeform_tags(self):
+        """
+        Gets the freeform_tags of this IPSecConnection.
+        Free-form tags for this resource. Each tag is a simple key-value pair with no
+        predefined name, type, or namespace. For more information, see
+        `Resource Tags`__.
+
+        Example: `{\"Department\": \"Finance\"}`
+
+        __ https://docs.us-phoenix-1.oraclecloud.com/Content/General/Concepts/resourcetags.htm
+
+
+        :return: The freeform_tags of this IPSecConnection.
+        :rtype: dict(str, str)
+        """
+        return self._freeform_tags
+
+    @freeform_tags.setter
+    def freeform_tags(self, freeform_tags):
+        """
+        Sets the freeform_tags of this IPSecConnection.
+        Free-form tags for this resource. Each tag is a simple key-value pair with no
+        predefined name, type, or namespace. For more information, see
+        `Resource Tags`__.
+
+        Example: `{\"Department\": \"Finance\"}`
+
+        __ https://docs.us-phoenix-1.oraclecloud.com/Content/General/Concepts/resourcetags.htm
+
+
+        :param freeform_tags: The freeform_tags of this IPSecConnection.
+        :type: dict(str, str)
+        """
+        self._freeform_tags = freeform_tags
 
     @property
     def id(self):
