5d12d2442a531b02141bb3e9d28ee65bde1bf1a2
Art Hall <art.hall.iii@oracle.com>
2018-06-16 07:59:38 -0400

    Pinning pytz to a lower end of 2016.10
    2016.10 was the original pinned version.
    All versions released on PyPi since then have now been tested
    using the OCI: Tests + Docs + Wheel TeamCity job.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ requires = [
     "PyJWT==1.5.3",
     "pyOpenSSL<=17.4.0",
     "python-dateutil==2.7.3",
-    "pytz==2018.4",
+    "pytz>=2016.10",
     "requests==2.18.4",
     "six==1.11.0",
 ]
