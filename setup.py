8bb4d03c9005350d9d7ed414a42374389e9b22a2
Art Hall Iii <art.hall.iii@oracle.com>
2018-07-03 19:50:54 +0000

    Merge pull request #688 in SDK/python-sdk from cryptography-version-testing to master
    Open cryptography pinning to 2.1.3 to 2.2.2
    
    Squashed commit of the following:
    
    commit 11ce73b138a1d1588c8ca8255155405b668e8d85
    Author: Art Hall <art.hall.iii@oracle.com>
    Date:   Mon Jul 2 18:17:44 2018 -0400
    
        Uncomment echo command which was still commented out when trying to run abreviated set of tests
    
    commit 7db90a59ae2cb593a6b6a71d68125f71e8ed2a60
    Author: Art Hall <art.hall.iii@oracle.com>
    Date:   Mon Jul 2 16:06:26 2018 -0400
    
        Open up pinning to allow cryptography 2.1.3 through 2.2.2
    
    commit ac86ef322102dfa69da61637e24a372dfa631543
    Author: Art Hall <art.hall.iii@oracle.com>
    Date:   Mon Jul 2 14:15:42 2018 -0400
    
        Cryptography 2.2.2
    
    commit cc9409cbb0ee32ea0ca6c0a082e809ce06979cfd
    Author: Art Hall <art.hall.iii@oracle.com>
    Date:   Mon Jul 2 13:06:38 2018 -0400
    
        Test cryptography 2.2.1
    
    commit 39b7c1f72d597b7bacf7111212af5d5c3d1ccb2f
    Author: Art Hall <art.hall.iii@oracle.com>
    Date:   Mon Jul 2 11:22:00 2018 -0400
    
        Cryptography 2.2 and turn on py27 tests again
    
    commit 3e6e1e206ace8f0a50d0dea562074a44ae9076c7
    Author: Art Hall <art.hall.iii@oracle.com>
    Date:   Mon Jul 2 10:10:33 2018 -0400
    
        Rebase on master and reenable building of the docs
    
    commit e70353369f0e2b9d7c99f6f017c98c58d1cb4d50
    Author: Art Hall <art.hall.iii@oracle.com>
    Date:   Wed Jun 27 17:11:31 2018 -0400
    
        Remove cassette for test_put_empty_file and only test py35
    
        This is another attempt to figure out why the object storage
        tests are failing.  Removed all extra parts of the build to
        speed up the process.
    
    commit 6289e85d5ed4ab284ad91afb7aa600244de97018
    Author: Art Hall <art.hall.iii@oracle.com>
    Date:   Mon Jun 25 13:53:14 2018 -0400
    
        Testing cryptography 2.1.4

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open_relative("README.rst") as f:
 requires = [
     "certifi",
     "configparser==3.5.0",
-    "cryptography==2.1.3",
+    "cryptography>=2.1.3,<=2.2.2",
     "idna>=2.5,<2.7",
     "pyOpenSSL<=17.4.0",
     "python-dateutil>=2.5.3,<=2.7.3",
