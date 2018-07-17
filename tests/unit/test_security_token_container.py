417fb64845b7d93b6ee7d1e3a1ee7f09316f7180
Nathan Vu <nathan.vu@oracle.com>
2018-07-17 02:32:03 +0000

    Merge pull request #695 in SDK/python-sdk from ~NVU/python-sdk:cherry-pick-instance-principals-retries to master
    Squashed commit of the following:
    
    commit 27b19dea3ff18b636b10055715e885366e3fc717
    Author: Nathan Vu <nathan.vu@oracle.com>
    Date:   Thu Jul 12 17:05:10 2018 -0700
    
        Fixup reference in tests
    
    commit db3909e6cf0d19c4161490a816d2f1a309c73aaf
    Author: Nathan Vu <nathan.vu@oracle.com>
    Date:   Tue Jul 3 16:26:47 2018 +0000
    
        Merge pull request #687 in SDK/python-sdk from ~NVU/python-sdk:preview-retry-cert-retriever-fed-client to preview
    
        Squashed commit of the following:
    
        commit 341e816b368e1152284efe1adad6f3a9f1473904
        Author: Nathan Vu <nathan.vu@oracle.com>
        Date:   Mon Jul 2 11:57:33 2018 -0700
    
            Add retries to Federation Client and URL-based certificate retriever
    
            Federation Client is used to communicate with Auth Service to get tokens for instance principals / S2S. The URL-based certificate retriever is
            used to fetch certificates from instance metadata on the host for instance principals (via Federation Client).
    
            We currently do not retry on failures with any of these. This change adds support for specifying retry strategies on these classes, but also
            puts in default retries since we don't expect customers to have to mess with it themselves (we just want them to create an instance principals signer and
            not have to worry about specifying extra parameters); the option is there for customers to mess with it, but we'll try and have (sensible) defaults so
            that they don't have to.
    
            Retries are as follows:
            * Federation Client uses the default retry strategy as we can treat Auth Service like any other OCI service
            * Instance metadata we blanket retry on any errors because it's not an OCI service
    
    commit 0afb2a947eeb22024b07efcfc7ae40c8d6d46a07
    Author: Nathan Vu <nathan.vu@oracle.com>
    Date:   Thu Jun 7 17:42:37 2018 +0000
    
        Merge pull request #650 in SDK/python-sdk from ~NVU/python-sdk:preview-inst-principal-token-expiry-with-jitter to preview
    
        Squashed commit of the following:
    
        commit e0068ef6e807f68490f2a3fa3e98efefcdd3ce1e
        Author: Nathan Vu <nathan.vu@oracle.com>
        Date:   Fri Jun 1 16:29:26 2018 -0700
    
            For instance principals tokens, add method to check validity with jitter
    
            Handles the scenario where the token was valid at the time of checking
            but just crossed over to being invalid at the time of the request being
            sent, as well as some minor clock skew forgiveness.
    
            More relevant when making raw requests as each SDK client has a catch
            block for 401s when using instance principals that should otherwise
            handle this

---
+++ tests/unit/test_security_token_container.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+# Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
+
+import oci._vendor.jwt as jwt
+import oci
+import pytest
+import time
+
+
+@pytest.fixture
+def valid_for_five_minutes_token():
+    time_now = int(time.time())
+    payload = {
+        'exp': time_now + 300,  # Expires 5 minutes in the future
+        'some_data': 'blah blah blah'
+    }
+
+    encoded = jwt.encode(payload, 'a_secret')
+    return {'exp': time_now, 'token': encoded}
+
+
+@pytest.fixture
+def expired_token():
+    time_now = int(time.time())
+    payload = {
+        'exp': time_now - 300,  # Expired 5 minutes ago
+        'some_data': 'blah blah blah'
+    }
+
+    encoded = jwt.encode(payload, 'a_secret')
+    return {'exp': time_now, 'token': encoded}
+
+
+def test_valid_without_jitter(valid_for_five_minutes_token):
+    security_token_container = oci.auth.security_token_container.SecurityTokenContainer(None, valid_for_five_minutes_token['token'])
+    assert security_token_container.valid()
+
+
+def test_valid_with_jitter(valid_for_five_minutes_token):
+    security_token_container = oci.auth.security_token_container.SecurityTokenContainer(None, valid_for_five_minutes_token['token'])
+    assert security_token_container.valid_with_jitter(1)
+    assert security_token_container.valid_with_jitter()
+    assert security_token_container.valid_with_jitter(299)
+    assert security_token_container.valid_with_jitter(300)
+    assert not security_token_container.valid_with_jitter(301)
+
+
+def test_invalid_without_jitter(expired_token):
+    security_token_container = oci.auth.security_token_container.SecurityTokenContainer(None, expired_token['token'])
+    assert not security_token_container.valid()
+
+
+def test_invalid_with_jitter(expired_token):
+    security_token_container = oci.auth.security_token_container.SecurityTokenContainer(None, expired_token['token'])
+    assert not security_token_container.valid_with_jitter(1)
+    assert not security_token_container.valid_with_jitter()
+    assert not security_token_container.valid_with_jitter(299)
+    assert not security_token_container.valid_with_jitter(300)
+    assert not security_token_container.valid_with_jitter(301)
