9490e78a47f2a9c95fcfd4df946621b004e61bde
Nathan Vu <nathan.vu@oracle.com>
2018-07-25 23:13:32 +0000

    Merge pull request #716 in SDK/python-sdk from ~NVU/python-sdk:fix-insecure-platform-warnings to master
    Squashed commit of the following:
    
    commit 4794009f6342453256826e56d58a172f364a55d7
    Author: Nathan Vu <nathan.vu@oracle.com>
    Date:   Wed Jul 25 16:01:27 2018 -0700
    
        Fix "InsecurePlatformWarning: A true SSLContext object..." error
    
        Impacts Python 2.7.x below 2.7.9 (there is a `HAS_SNI` attribute in 2.7.9 that urllib3 expects to be there). This hits on the image
        tests because one of the things they test is the lowest supported Python version we declare (which happens to be 2.7.5).
    
        Normally using Python 2.7.5 is not a big deal, but the patching of OpenSSL via PyOpenSSL doesn't work post-vendoring because the "from ..."
        statement is incorrect. Thus we start throwing errors.
    
        This isn't showing up in the normal build job, but I'm assuming that is because:
    
        * We mainly use recordings and once VCRPy intercepts the requests it doesn't care whether there's a valid context or not (since it'll never use it)
        * Warnings aren't showing up for tests that do hit the real service because of some STDOUT/STDERR suppression

--- src/oci/_vendor/requests/__init__.py
+++ src/oci/_vendor/requests/__init__.py
@@ -85,7 +85,7 @@ except (AssertionError, ValueError):
 
 # Attempt to enable urllib3's SNI support, if possible
 try:
-    from urllib3.contrib import pyopenssl
+    from oci._vendor.urllib3.contrib import pyopenssl
     pyopenssl.inject_into_urllib3()
 except ImportError:
     pass
