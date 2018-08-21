62df2380bc14e56e4a17434418950be4cca8e507
Art Hall <art.hall.iii@oracle.com>
2018-08-21 19:47:33 -0400

    Add entry for CreateAutonomousDatabaseDetails to test_init_from_kwargs
    New model CreateAutonomousDatabaseDetails only accepts specific values for license_model.

--- tests/unit/test_init_from_kwargs.py
+++ tests/unit/test_init_from_kwargs.py
@@ -16,6 +16,7 @@ ENUM_ATTR_TO_VALUE = {
     'LaunchDbSystemDetails': {'source': 'DB_BACKUP', 'license_model': 'LICENSE_INCLUDED', 'disk_redundancy': 'NORMAL', 'database_edition': 'STANDARD_EDITION'},
     'LaunchDbSystemFromBackupDetails': {'source': 'DB_BACKUP', 'license_model': 'LICENSE_INCLUDED', 'disk_redundancy': 'NORMAL', 'database_edition': 'STANDARD_EDITION'},
     'CreateAutonomousDataWarehouseDetails': {'license_model': 'LICENSE_INCLUDED'},
+    'CreateAutonomousDatabaseDetails': {'license_model': 'LICENSE_INCLUDED'},
     'CreateDatabaseDetails': {'db_workload': 'OLTP'},
     'CreateDataGuardAssociationDetails': {'protection_mode': 'MAXIMUM_AVAILABILITY', 'transport_type': 'SYNC'},
     'CreateDataGuardAssociationToExistingDbSystemDetails': {'protection_mode': 'MAXIMUM_AVAILABILITY', 'transport_type': 'SYNC'},
