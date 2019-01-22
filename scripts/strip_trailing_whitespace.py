aff6c0b936af528eb5df59f154c2f1b37f676471
Art Hall <art.hall.iii@oracle.com>
2019-01-22 17:37:18 -0500

    fix import error for idna
    The newest version of cryptography, 2.5.0 no longer requires idna.
    The requirement from cryptography was hiding the fact the vendored
    urllib3 package was importing idna.  Change the import inside
    urllib3 to import the vendored version of idna.
    
    Tested this by setting up a new virtualenv for 2.7.12, 3.5.1 and
    3.6.5 and running `pip install oci`.  In all venvs there was an
    import exception when trying to make an API call.  In all venvs
    used pip to install from this code base `pip install -e path_to_sdk`
    and reran the test to make an API call.  In all cases this change
    fixed the import exception.

--- scripts/strip_trailing_whitespace.py
+++ scripts/strip_trailing_whitespace.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
 # This script runs at the end of the code generation process and strips any trailing
-# whitespace (which would otherwise cause Rubocop to complain). This is a bit hacky
+# whitespace (which would otherwise cause Flake8 to complain). This is a bit hacky
 # but it's easier than messing around with mustache templates
 
 import os
 import os.path
 
 SOURCE_CODE_LOCAITON = 'src/oci'
+CODE_EXTENTIONS = ['.py']
 
 for dirpath, dirnames, filenames in os.walk(SOURCE_CODE_LOCAITON):
     for f in filenames:
+        _, extention = os.path.splitext(f)
+        if extention not in CODE_EXTENTIONS:
+            continue
         file_path = os.path.join(dirpath, f)
         print('Stripping whitespace from {}'.format(file_path))
         with open(file_path, 'r') as file:
