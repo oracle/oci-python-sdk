92d1b325b255c17e40dcc0bc8f2342d3325785ff
DEXREQ Automation <noreply+dexreq@oracle.com>
2019-04-09 22:35:49 +0000

    Author: DEXREQ Automation <noreply+dexreq@oracle.com> Date:  Tue, 9 Apr 2019 22:35:49 +0000 Running generation for: [[DEXREQ-427]]
    Author: DEXREQ Automation <noreply+dexreq@oracle.com>
    Date:  Tue, 9 Apr 2019 22:11:06 +0000
    Updating pom.xml for [[DEXREQ-427]]: Public SDK for UDX-1789: Identity: Adding tagging support for dynamic groups
    
    (automatically combined)

--- src/oci/identity/models/create_dynamic_group_details.py
+++ src/oci/identity/models/create_dynamic_group_details.py
@@ -33,25 +33,39 @@ class CreateDynamicGroupDetails(object):
             The value to assign to the description property of this CreateDynamicGroupDetails.
         :type description: str
 
+        :param freeform_tags:
+            The value to assign to the freeform_tags property of this CreateDynamicGroupDetails.
+        :type freeform_tags: dict(str, str)
+
+        :param defined_tags:
+            The value to assign to the defined_tags property of this CreateDynamicGroupDetails.
+        :type defined_tags: dict(str, dict(str, object))
+
         """
         self.swagger_types = {
             'compartment_id': 'str',
             'name': 'str',
             'matching_rule': 'str',
-            'description': 'str'
+            'description': 'str',
+            'freeform_tags': 'dict(str, str)',
+            'defined_tags': 'dict(str, dict(str, object))'
         }
 
         self.attribute_map = {
             'compartment_id': 'compartmentId',
             'name': 'name',
             'matching_rule': 'matchingRule',
-            'description': 'description'
+            'description': 'description',
+            'freeform_tags': 'freeformTags',
+            'defined_tags': 'definedTags'
         }
 
         self._compartment_id = None
         self._name = None
         self._matching_rule = None
         self._description = None
+        self._freeform_tags = None
+        self._defined_tags = None
 
     @property
     def compartment_id(self):
@@ -156,6 +170,70 @@ class CreateDynamicGroupDetails(object):
         :type: str
         """
         self._description = description
+
+    @property
+    def freeform_tags(self):
+        """
+        Gets the freeform_tags of this CreateDynamicGroupDetails.
+        Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace.
+        For more information, see `Resource Tags`__.
+        Example: `{\"Department\": \"Finance\"}`
+
+        __ https://docs.cloud.oracle.com/Content/General/Concepts/resourcetags.htm
+
+
+        :return: The freeform_tags of this CreateDynamicGroupDetails.
+        :rtype: dict(str, str)
+        """
+        return self._freeform_tags
+
+    @freeform_tags.setter
+    def freeform_tags(self, freeform_tags):
+        """
+        Sets the freeform_tags of this CreateDynamicGroupDetails.
+        Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace.
+        For more information, see `Resource Tags`__.
+        Example: `{\"Department\": \"Finance\"}`
+
+        __ https://docs.cloud.oracle.com/Content/General/Concepts/resourcetags.htm
+
+
+        :param freeform_tags: The freeform_tags of this CreateDynamicGroupDetails.
+        :type: dict(str, str)
+        """
+        self._freeform_tags = freeform_tags
+
+    @property
+    def defined_tags(self):
+        """
+        Gets the defined_tags of this CreateDynamicGroupDetails.
+        Defined tags for this resource. Each key is predefined and scoped to a namespace.
+        For more information, see `Resource Tags`__.
+        Example: `{\"Operations\": {\"CostCenter\": \"42\"}}`
+
+        __ https://docs.cloud.oracle.com/Content/General/Concepts/resourcetags.htm
+
+
+        :return: The defined_tags of this CreateDynamicGroupDetails.
+        :rtype: dict(str, dict(str, object))
+        """
+        return self._defined_tags
+
+    @defined_tags.setter
+    def defined_tags(self, defined_tags):
+        """
+        Sets the defined_tags of this CreateDynamicGroupDetails.
+        Defined tags for this resource. Each key is predefined and scoped to a namespace.
+        For more information, see `Resource Tags`__.
+        Example: `{\"Operations\": {\"CostCenter\": \"42\"}}`
+
+        __ https://docs.cloud.oracle.com/Content/General/Concepts/resourcetags.htm
+
+
+        :param defined_tags: The defined_tags of this CreateDynamicGroupDetails.
+        :type: dict(str, dict(str, object))
+        """
+        self._defined_tags = defined_tags
 
     def __repr__(self):
         return formatted_flat_dict(self)
