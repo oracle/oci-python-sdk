f8d9faaca6ad4b7ca000db4d080dd3f5e21ade4f
Art Hall III <art.hall.iii@oracle.com>
2019-07-01 18:35:47 +0000

    DEXREQ-376 : DBAAS-11191 Add python example to update adb license type. DEXREQ-419: DBAAS-11190 Add updateNetworkAcl python sample to python SDK
    Squashed commit of the following:
    
    commit 6afe4c7d3e982a2de3520e8d7cde8d129375ef61
    Author: Shuo Li <shuo.sl.li@oracle.com>
    Date:   Tue Jun 25 15:40:23 2019 -0700
    
        New line at the end
    
    commit 3196fd9a11aad93ac991a02eb9812697f871a63d
    Author: Shuo Li <shuo.sl.li@oracle.com>
    Date:   Mon May 20 22:50:09 2019 -0700
    
        DEXREQ-419: DBAAS-11190 Add updateNetworkAcl python sample to python SDK
    
    commit c9ff3333a08e2c45980ba8fe4c72f792a727d88d
    Author: Shuo Li <shuo.sl.li@oracle.com>
    Date:   Wed Jun 19 12:31:16 2019 -0700
    
        DEXREQ-376 : DBAAS-11191 Add python example to update adb license type

--- examples/database/adb_example.py
+++ examples/database/adb_example.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2016, 2019, Oracle and/or its affiliates. All rights reserved.
 
 import oci
+import time
 
 # Overview of Autonomous Data Warehouse
 # https://docs.cloud.oracle.com/iaas/Content/Database/Concepts/adboverview.htm
@@ -26,6 +27,8 @@ def create_adb(db_client):
     adb_request.display_name = "PYSDK-ADB-EXAMPLE"
     adb_request.db_workload = "OLTP"
     adb_request.license_model = adb_request.LICENSE_MODEL_BRING_YOUR_OWN_LICENSE
+    # For demonstration, we just passed the password here but for Production code you should have a better
+    # way to pass the password like env variable or commandline
     adb_request.admin_password = "Welcome1!SDK"
     adb_request.is_auto_scaling_enabled = True
 
@@ -38,8 +41,8 @@ def create_adb(db_client):
     return adb_response.data.id
 
 
+# Delete the autonomous database
 def delete_adb(db_client, adb_id):
-    # Delete the autonomous database
     response = db_client.delete_autonomous_database(adb_id)
     print(response)
 
@@ -62,8 +65,50 @@ def update_adb(db_client, adb_id):
     return adb_response.data.id
 
 
+def update_adb_acl(db_client, adb_id):
+    # Update adb Access Control List
+    adb_request = oci.database.models.UpdateAutonomousDatabaseDetails()
+
+    adb_request.whitelisted_ips = ["1.1.1.1/28", "3.3.3.3"]
+
+    adb_response = db_client.update_autonomous_database(adb_id,
+                                                        update_autonomous_database_details=adb_request,
+                                                        retry_strategy=oci.retry.DEFAULT_RETRY_STRATEGY)
+
+    print("ATP Shared instance Access Control List is being changed {}".format(adb_response.data.id))
+
+    return adb_response.data.id
+
+
+def update_adb_licesnse_type(db_client, adb_id):
+    # Update adb Licesnse Type
+    adb_request = oci.database.models.UpdateAutonomousDatabaseDetails()
+
+    adb_request.license_model = adb_request.LICENSE_MODEL_LICENSE_INCLUDED
+
+    adb_response = db_client.update_autonomous_database(adb_id,
+                                                        update_autonomous_database_details=adb_request,
+                                                        retry_strategy=oci.retry.DEFAULT_RETRY_STRATEGY)
+
+    print("ATP Shared instance License Type is being changed {}".format(adb_response.data.id))
+
+    return adb_response.data.id
+
+
 if __name__ == "__main__":
     # Initialize the client
     db_client = oci.database.DatabaseClient(config)
+    # Create adb
     adb_id = create_adb(db_client)
+    # sleep 5 mins
+    time.sleep(300)
+    # Update adb acl with specific adb_id.
+    update_adb_acl(db_client, adb_id)
+    # sleep 5 mins
+    time.sleep(300)
+    # Update adb license type
+    update_adb_licesnse_type(db_client, adb_id)
+    # sleep 5 mins
+    time.sleep(300)
+    # Delete adb
     delete_adb(db_client, adb_id)
