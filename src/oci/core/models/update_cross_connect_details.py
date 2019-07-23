d663cbdd56c9a6c1eb293400e0fca0e6467eda73
DEXREQ Automation <noreply+dexreq@oracle.com>
2019-07-23 17:42:30 +0000

    Author: DEXREQ Automation <noreply+dexreq@oracle.com> Date:  Tue, 23 Jul 2019 17:42:30 +0000 Running generation for: [[DEXREQ-652]]
    Author: DEXREQ Automation <noreply+dexreq@oracle.com>
    Date:  Tue, 23 Jul 2019 17:15:36 +0000
    Updating pom.xml for [[DEXREQ-652]]: Public SDK for UDX-824:Adding tagging support to VPN APIs
    
    (automatically combined)

--- src/oci/core/models/update_cross_connect_details.py
+++ src/oci/core/models/update_cross_connect_details.py
@@ -17,9 +17,17 @@ class UpdateCrossConnectDetails(object):
         Initializes a new UpdateCrossConnectDetails object with values from keyword arguments.
         The following keyword arguments are supported (corresponding to the getters/setters of this class):
 
+        :param defined_tags:
+            The value to assign to the defined_tags property of this UpdateCrossConnectDetails.
+        :type defined_tags: dict(str, dict(str, object))
+
         :param display_name:
             The value to assign to the display_name property of this UpdateCrossConnectDetails.
         :type display_name: str
+
+        :param freeform_tags:
+            The value to assign to the freeform_tags property of this UpdateCrossConnectDetails.
+        :type freeform_tags: dict(str, str)
 
         :param is_active:
             The value to assign to the is_active property of this UpdateCrossConnectDetails.
@@ -31,20 +39,60 @@ class UpdateCrossConnectDetails(object):
 
         """
         self.swagger_types = {
+            'defined_tags': 'dict(str, dict(str, object))',
             'display_name': 'str',
+            'freeform_tags': 'dict(str, str)',
             'is_active': 'bool',
             'customer_reference_name': 'str'
         }
 
         self.attribute_map = {
+            'defined_tags': 'definedTags',
             'display_name': 'displayName',
+            'freeform_tags': 'freeformTags',
             'is_active': 'isActive',
             'customer_reference_name': 'customerReferenceName'
         }
 
+        self._defined_tags = None
         self._display_name = None
+        self._freeform_tags = None
         self._is_active = None
         self._customer_reference_name = None
+
+    @property
+    def defined_tags(self):
+        """
+        Gets the defined_tags of this UpdateCrossConnectDetails.
+        Defined tags for this resource. Each key is predefined and scoped to a
+        namespace. For more information, see `Resource Tags`__.
+
+        Example: `{\"Operations\": {\"CostCenter\": \"42\"}}`
+
+        __ https://docs.cloud.oracle.com/Content/General/Concepts/resourcetags.htm
+
+
+        :return: The defined_tags of this UpdateCrossConnectDetails.
+        :rtype: dict(str, dict(str, object))
+        """
+        return self._defined_tags
+
+    @defined_tags.setter
+    def defined_tags(self, defined_tags):
+        """
+        Sets the defined_tags of this UpdateCrossConnectDetails.
+        Defined tags for this resource. Each key is predefined and scoped to a
+        namespace. For more information, see `Resource Tags`__.
+
+        Example: `{\"Operations\": {\"CostCenter\": \"42\"}}`
+
+        __ https://docs.cloud.oracle.com/Content/General/Concepts/resourcetags.htm
+
+
+        :param defined_tags: The defined_tags of this UpdateCrossConnectDetails.
+        :type: dict(str, dict(str, object))
+        """
+        self._defined_tags = defined_tags
 
     @property
     def display_name(self):
@@ -71,6 +119,40 @@ class UpdateCrossConnectDetails(object):
         :type: str
         """
         self._display_name = display_name
+
+    @property
+    def freeform_tags(self):
+        """
+        Gets the freeform_tags of this UpdateCrossConnectDetails.
+        Free-form tags for this resource. Each tag is a simple key-value pair with no
+        predefined name, type, or namespace. For more information, see `Resource Tags`__.
+
+        Example: `{\"Department\": \"Finance\"}`
+
+        __ https://docs.cloud.oracle.com/Content/General/Concepts/resourcetags.htm
+
+
+        :return: The freeform_tags of this UpdateCrossConnectDetails.
+        :rtype: dict(str, str)
+        """
+        return self._freeform_tags
+
+    @freeform_tags.setter
+    def freeform_tags(self, freeform_tags):
+        """
+        Sets the freeform_tags of this UpdateCrossConnectDetails.
+        Free-form tags for this resource. Each tag is a simple key-value pair with no
+        predefined name, type, or namespace. For more information, see `Resource Tags`__.
+
+        Example: `{\"Department\": \"Finance\"}`
+
+        __ https://docs.cloud.oracle.com/Content/General/Concepts/resourcetags.htm
+
+
+        :param freeform_tags: The freeform_tags of this UpdateCrossConnectDetails.
+        :type: dict(str, str)
+        """
+        self._freeform_tags = freeform_tags
 
     @property
     def is_active(self):
