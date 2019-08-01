9fa38bf6816ad1fbd5db85a4db264fc731bd5d9f
Art Hall <art.hall.iii@oracle.com>
2019-08-01 15:58:24 -0400

    Add recording for test_user_crud
    Working on one of the auto generated branches I noticed the test_user_crud
    test failed because python_temp_user_vcr already existed.  This user shouldn't
    be created in the tenancy because the test should be running against a recording.
    The name even suggests it's using a recording, but the test wasn't using a
    recording which could lead to timing issues when running multiple builds.
    
    (cherry picked from commit 5a645006d282dabc157087fb9cbd1c5596ea9756)

--- tests/unit/test_user_crud.py
+++ tests/unit/test_user_crud.py
@@ -3,52 +3,54 @@
 
 import oci
 import tests.util
+from .. import test_config_container
 
 
 def test_user_crud(identity, config):
-    compartment = config["tenancy"]
-    user_name = tests.util.unique_name("python_temp_user")
-    user_description = "Created by python SDK TestUserCrud test."
+    with test_config_container.create_vcr().use_cassette('test_user_crud.yml'):
+        compartment = config["tenancy"]
+        user_name = tests.util.unique_name("python_temp_user")
+        user_description = "Created by python SDK TestUserCrud test."
 
-    # Create User
-    request = oci.identity.models.CreateUserDetails()
-    request.compartment_id = compartment
-    request.name = user_name
-    request.description = user_description
+        # Create User
+        request = oci.identity.models.CreateUserDetails()
+        request.compartment_id = compartment
+        request.name = user_name
+        request.description = user_description
 
-    response = identity.create_user(request)
+        response = identity.create_user(request)
 
-    assert type(response.data) is oci.identity.models.User
-    assert user_name == response.data.name
-    assert user_description == response.data.description
-    assert compartment == response.data.compartment_id
-    user_id = response.data.id
+        assert type(response.data) is oci.identity.models.User
+        assert user_name == response.data.name
+        assert user_description == response.data.description
+        assert compartment == response.data.compartment_id
+        user_id = response.data.id
 
-    all_users = list_all_users(identity, config)
-    assert search_non_deleted_user_in_list(user_id=user_id, user_list=all_users, should_find=True)
+        all_users = list_all_users(identity, config)
+        assert search_non_deleted_user_in_list(user_id=user_id, user_list=all_users, should_find=True)
 
-    # Get User
-    response = identity.get_user(user_id)
-    assert type(response.data) is oci.identity.models.User
-    assert user_name == response.data.name
-    assert user_description == response.data.description
-    assert compartment == response.data.compartment_id
+        # Get User
+        response = identity.get_user(user_id)
+        assert type(response.data) is oci.identity.models.User
+        assert user_name == response.data.name
+        assert user_description == response.data.description
+        assert compartment == response.data.compartment_id
 
-    # Update User
-    new_description = "updated user description"
-    request = oci.identity.models.UpdateUserDetails()
-    request.description = new_description
-    response = identity.update_user(user_id, request)
+        # Update User
+        new_description = "updated user description"
+        request = oci.identity.models.UpdateUserDetails()
+        request.description = new_description
+        response = identity.update_user(user_id, request)
 
-    assert type(response.data) is oci.identity.models.User
-    assert user_name == response.data.name
-    assert new_description == response.data.description
-    assert compartment == response.data.compartment_id
+        assert type(response.data) is oci.identity.models.User
+        assert user_name == response.data.name
+        assert new_description == response.data.description
+        assert compartment == response.data.compartment_id
 
-    # Delete User
-    identity.delete_user(user_id)
-    all_users = list_all_users(identity, config)
-    assert search_non_deleted_user_in_list(user_id=user_id, user_list=all_users, should_find=False)
+        # Delete User
+        identity.delete_user(user_id)
+        all_users = list_all_users(identity, config)
+        assert search_non_deleted_user_in_list(user_id=user_id, user_list=all_users, should_find=False)
 
 
 def search_non_deleted_user_in_list(user_id, user_list, should_find=True):
