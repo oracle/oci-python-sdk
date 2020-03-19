45faf2d544bc6fe2b1930a96bbdf121ecaabdb18
Vyas Bhagwat <vyas.bhagwat@oracle.com>
2020-03-19 13:06:19 -0700

    Handle binary content-type

---
+++ tests/unit/test_add_headers.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+# Copyright (c) 2016, 2020, Oracle and/or its affiliates. All rights reserved.
+
+from oci._vendor.requests.models import Request
+from oci._vendor.requests.sessions import Session, HTTPAdapter
+
+
+def test_add_headers():
+    headers = {
+        'accept': 'application/json',
+        'opc-client-info': 'Oracle-PythonSDK/2.11.1+preview+1',
+        'user-agent': 'Oracle-PythonSDK/2.11.1+preview.1 (python 3.6.5; x86_64-Darwin)',
+        'opc-request-id': '996FE54D0755404BA477E4CC852819F0'
+    }
+    request = Request(
+        method="POST",
+        url='https://datascience.us-phoenix-1.oci.oraclecloud.com/20190101/models/ocid1.datasciencemodel.oc1.phx.amaaaaaaev43fkiaswn7lo3fgcyc3wke3rbcxenp7x4pjtkgw2u65ob2wv7q/artifact',
+        headers=headers
+    )
+    session = Session()
+    adapter = HTTPAdapter()
+    prepared_request = session.prepare_request(request)
+    adapter.add_headers(prepared_request)
+    assert prepared_request.headers['content-type'] == 'application/octet-stream'
