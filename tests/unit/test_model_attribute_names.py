57b2f1aee3f36882948e4f07f884a3176b4882a0
Vyas Bhagwat <vyas.bhagwat@oracle.com>
2020-07-16 20:32:31 +0000

    Merge pull request #2293 in SDK/python-sdk from regen/M-DEX-9343 to master
    Squashed commit of the following:
    
    commit a1fb02aee03119bddf9a78e72e28ba1d514a2aa3
    Author: Vyas Bhagwat <vyas.bhagwat@oracle.com>
    Date:   Wed Jul 15 11:20:01 2020 -0700
    
        DEX-9343: Regen after codegen fix, then use renaming plugin to revert changes to properties which exist before 7/10 bulk preview to avoid breaking changes
    
        Bump codegen version
    
        Modify test for attribute names

--- tests/unit/test_model_attribute_names.py
+++ tests/unit/test_model_attribute_names.py
@@ -4,15 +4,35 @@
 
 import oci
 
-invalid_conversions = ['size_in_g_bs',
-                       'size_in_m_bs',
-                       'size_in_t_bs',
-                       'size_in_k_bs',
-                       'size_in_p_bs']
+invalid_conversions = ['in_g_bs',
+                       'in_m_bs',
+                       'in_t_bs',
+                       'in_k_bs',
+                       'in_p_bs']
+
+# Ignore some attributes for this test.
+# We found this bug a little too late and these properties already exist in the public SDK
+# Instead of re-gening with the correct name and have breaking changes, it is better to leave them unchanged
+ignored_attributes = (("blockchain", "BlockchainPlatform", "storage_used_in_t_bs"),
+                      ("core", "ShapeMemoryOptions", "min_in_g_bs"),
+                      ("core", "ShapeMemoryOptions", "max_in_g_bs"),
+                      ("core", "ShapeMemoryOptions", "default_per_ocpu_in_g_bs"),
+                      ("database", "DbSystemShapeSummary", "min_memory_per_node_in_g_bs"),
+                      ("database", "DbSystemShapeSummary", "available_db_node_storage_in_g_bs"),
+                      ("database", "DbSystemShapeSummary", "min_db_node_storage_per_node_in_g_bs"),
+                      ("database", "DbSystemShapeSummary", "available_data_storage_in_t_bs"),
+                      ("database", "DbSystemShapeSummary", "min_data_storage_in_t_bs"),
+                      ("database", "ExadataInfrastructure", "max_db_node_storage_in_g_bs"),
+                      ("database", "ExadataInfrastructure", "max_data_storage_in_t_bs"),
+                      ("database", "ExadataInfrastructureSummary", "max_db_node_storage_in_g_bs"),
+                      ("database", "ExadataInfrastructureSummary", "max_data_storage_in_t_bs"),
+                      ("nosql", "QueryDetails", "max_read_in_k_bs"),
+                      ("nosql", "TableLimits", "max_storage_in_g_bs"),
+                      ("nosql", "TableUsageSummary", "storage_in_g_bs"))
 
 
 def test_bad_conversions_in_attibute_names():
-    # Programmitically get all the models by inspecting all of the modules
+    # Programmatically get all the models by inspecting all of the modules
     # in oci.
     for x in dir(oci):
         # oci.version is a module and we want to find all the other modules
@@ -20,5 +40,8 @@ def test_bad_conversions_in_attibute_names():
         if isinstance(instance_x, type(oci.version)) and hasattr(instance_x, 'models'):
             for model_name in dir((instance_x).models):
                 for attribute in dir(eval("{}.models.{}".format(instance_x.__name__, model_name))):
+                    # Continue for attributes in ignore list
+                    if (x, model_name, attribute) in ignored_attributes:
+                        continue
                     for invalid in invalid_conversions:
                         assert(invalid not in attribute)
