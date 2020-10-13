2b73cd8e8c6874caaeaaf61e87d6b422328fab30
Hongjian Niu <hongjian.niu@oracle.com>
2020-10-13 15:03:17 -0700

    Add comment back

--- examples/usage_api_example.py
+++ examples/usage_api_example.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 # Copyright (c) 2016, 2020, Oracle and/or its affiliates.  All rights reserved.
 # This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
+# This script provides a basic example of how to you can use usage api.
 # It then shows how to perform updates, reads, and deletes. It will:
 #   * Create a usage api client
 #   * get possible parameters for querying usage/cost
