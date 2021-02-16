abc1f9f61dfba6ada17e6f9e4fd2ce05f7e935fc
Walt Tran <walt.tran@oracle.com>
2021-02-16 21:45:25 +0000

    Merge pull request #2612 in SDK/python-sdk from DEX-10831 to preview
    Squashed commit of the following:
    
    commit 9ccf1297541118d607e4b19b564515aa420d437b
    Author: Walt Tran <11575468+waruwaruwaru@users.noreply.github.com>
    Date:   Fri Jan 29 10:49:56 2021 -0800
    
        Add support for appending ENV variable to user agent

--- src/oci/base_client.py
+++ src/oci/base_client.py
@@ -30,8 +30,12 @@ from .response import Response
 from .version import __version__
 from .util import NONE_SENTINEL, Sentinel
 missing = Sentinel("Missing")
-
+APPEND_USER_AGENT_ENV_VAR_NAME = "OCI_SDK_APPEND_USER_AGENT"
+APPEND_USER_AGENT = os.environ.get(APPEND_USER_AGENT_ENV_VAR_NAME)
 USER_INFO = "Oracle-PythonSDK/{}".format(__version__)
+
+if APPEND_USER_AGENT:
+    USER_INFO += " {}".format(APPEND_USER_AGENT)
 
 DICT_VALUE_TYPE_REGEX = re.compile('dict\(str, (.+?)\)$')  # noqa: W605
 LIST_ITEM_TYPE_REGEX = re.compile('list\[(.+?)\]$')  # noqa: W605
