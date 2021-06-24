b7d25ed37bca7372b1bb4ea7be37801e046f8194
Adi Zohar <adi.zohar@oracle.com>
2021-06-24 12:29:33 -0400

    SHOWOCI reporting tool for OCI Ver 21.06.29a

--- examples/showoci/showoci.py
+++ examples/showoci/showoci.py
@@ -95,7 +95,7 @@ import sys
 import argparse
 import datetime
 
-version = "21.06.02"
+version = "21.06.29"
 
 ##########################################################################
 # check OCI version
