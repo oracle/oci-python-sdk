9861df46698218c2071b62c429aa2b2b5016b5d9
Vyas Bhagwat <vyas.bhagwat@oracle.com>
2022-08-24 23:27:08 +0000

    Merge pull request #3550 in SDK/python-sdk from ~JBIARD/python-sdk:DBTOOLS-1359_mysql_examples to master
    Squashed commit of the following:
    
    commit c3c3ddfe443346719e243827662db060e19737f4
    Author: jbiard <justin.biard@oracle.com>
    Date:   Fri Aug 19 10:02:17 2022 -0500
    
        removing redundant type string
    
    commit b5027077d96838d41e6a1926361f3e960eb80142
    Author: jbiard <justin.biard@oracle.com>
    Date:   Wed Aug 17 12:48:25 2022 -0500
    
        updates from final testing
    
    commit abbff3ab8951e6bb27015005e9b626134532a1ed
    Author: jbiard <justin.biard@oracle.com>
    Date:   Wed Aug 17 11:48:22 2022 -0500
    
        cleanup prints, asserts, and comments
    
    commit 594326dd7e0bde22c3c4228078f12a8142ceefbe
    Author: jbiard <justin.biard@oracle.com>
    Date:   Tue Aug 16 16:58:58 2022 -0500
    
        cleanup indentation with early returns
    
    commit f3f739cd747d8688d3a76aedd23321278eae59d3
    Author: jbiard <justin.biard@oracle.com>
    Date:   Tue Aug 16 16:09:09 2022 -0500
    
        clarify create_secret
    
    commit 8cc9898c110aca1299d69c1a3ad49aa7131ac12c
    Author: jbiard <justin.biard@oracle.com>
    Date:   Tue Aug 16 15:49:28 2022 -0500
    
        add global variables for user setup
    
    commit 46188305313ca34a8b51ca8658d385fde8caa2a0
    Author: jbiard <justin.biard@oracle.com>
    Date:   Tue Aug 16 13:22:56 2022 -0500
    
        rename to terminal_states
    
    commit bddbede3237f5cf674d3e308736a0c49e6f78b9c
    Author: jbiard <justin.biard@oracle.com>
    Date:   Mon Aug 15 23:01:02 2022 -0500
    
        word smithing, cleanup, and updates per feedback.
    
    commit 60790a7e91517cec7815cd7dd9b18631d48731af
    Author: jbiard <justin.biard@oracle.com>
    Date:   Fri Aug 12 19:46:09 2022 -0500
    
        refactor existing and add new examples

--- examples/database_tools/mysql_create_connection_public_ip.py
+++ examples/database_tools/mysql_create_connection_public_ip.py
@@ -3,40 +3,44 @@
 # This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
 
 # -----------------------------------------------------------------------------
-# Example Use Case: Existing ADB-S with Public IP (no ACL)
+# Example Use Case: Existing MySQL Database with Public IP (customer-managed)
 # -----------------------------------------------------------------------------
-# This example creates a Database Tools connection to an Autonomous Database
-# on shared Exadata infrastructure (ADB-S), accessible by public ip. Note, since
-# this connection will be against a public ip address, a Database Tools private
-# endpoint is not required, however, mTLS is required.
+# This example creates a Database Tools connection to a MySQL database
+# accessible by public ip. Note, since this connection will be against a
+# public ip address, a Database Tools private endpoint is not required.
+# Exposing a database directly to the Internet is NOT a recommended practice
+# for security reasons. This example serves as an academic exercise of the SDK.
 #
 # Prerequisites:
 #
-#  - A compartment where Autonomous Database, vault, and connection will reside
-#  - An existing vault for storage of secrets, with at least one master key
+#  - An existing MySQL database on a compute node, for example
+#  - Firewall or security list entries allowing TCP traffic to MySQL
+#  - An existing Vault for storage of secrets, with at least one master key.
 #  - A previously configured .oci/config file with a [DEFAULT] section
 #  - Set the following variables in the code below:
 #      + compartment_id    : The compartment to use with the example
 #      + vault_id          : The ocid for a vault (to store secrets)
-#      + db_password       : The database password for the ADMIN user
+#      + db_username       : The MySQL database user to connect with
+#      + db_password       : The MySQL database password to connect with
+#      + connection_string : The MySQL connection string
 #
 # High-level Steps:
 #
-#  1- Create a new Autonomous Database (ADB-S)
-#  2- Create required secrets
-#  3- Create a connection
-#  4- Validate the connection
+#  1- Create required secret
+#  2- Create a connection
+#  3- Validate the connection
 # -----------------------------------------------------------------------------
 
 import oci
 from oci.config import from_file, validate_config
-from utils.dbtools import get_dbtools_clients, get_database_clients, get_secrets_client, get_kms_vault_client
-from utils.dbtools import get_vaults_clients, get_wallet_and_create_secrets, delete_secret
+from utils.dbtools import get_dbtools_clients, get_secrets_client, get_kms_vault_client, get_vaults_clients
+from utils.dbtools import create_secret, delete_secret
 
 import pkg_resources
+import base64
 from datetime import datetime, timedelta
 
-# Set do_clean_up_at_end = False to keep the secrets and connection created
+# Set do_clean_up_at_end = False to keep the secret and connection created
 do_clean_up_at_end = True
 
 # Specify compartment ocid to use. (can also use tenancy ocid if applicable)
@@ -45,7 +49,11 @@ compartment_id = "ocid1.tenancy......changeme"
 # Specify vault ocid to use. (For this example, vault must be in compartment_id and contain a master key)
 vault_id = "ocid1.vault.....changeme"
 
-# Specify the password for the ADB-S database ADMIN user
+# Specify the MySQL database connection string
+connection_string = "mysql://host.changeme:port/"
+
+# Specify the database username and password
+db_username = "example-username"
 db_password = "example-password"
 
 print("Using oci version:", pkg_resources.get_distribution("oci").version)
@@ -55,151 +63,74 @@ class DBToolsExample:
     """
     A Database Tools example that demonstrates usage of the OCI Python SDK.
     """
-    def __init__(self, compartment, vault, password):
+    def __init__(self, compartment, vault, connection_string, username, password):
 
         # Load an OCI config with the DEFAULT profile
         self.oci_config = from_file(file_location="~/.oci/config", profile_name="DEFAULT")
 
         self.compartment_id = compartment
         self.vault_id = vault
+        self.connection_string = connection_string
+        self.db_username = username
         self.db_password = password
-
-        self.db_name = "DB" + datetime.utcnow().strftime("%m%d%H%M")
 
         validate_config(self.oci_config)
 
         # Prepare SDK clients used in this example
         self.dbtools_client, self.dbtools_async_client = get_dbtools_clients(self.oci_config)
-        self.db_client, self.db_async_client = get_database_clients(self.oci_config)
         self.vaults_client, self.vaults_async_client = get_vaults_clients(self.oci_config)
         self.kms_vault_client = get_kms_vault_client(self.oci_config)
         self.secrets_client = get_secrets_client(self.oci_config)
 
         # Dict that contains all the clients (used by utils.dbtools helpers)
-        self.clients = {"dbtools_client": self.dbtools_client, "dbtools_async_client": self.dbtools_async_client,
-                        "db_client": self.db_client, "db_async_client": self.db_async_client,
-                        "vaults_client": self.vaults_client, "vaults_async_client": self.vaults_async_client,
+        self.clients = {"vaults_client": self.vaults_client, "vaults_async_client": self.vaults_async_client,
                         "kms_vault_client": self.kms_vault_client,
-                        "secrets_client": self.secrets_client}
+                        "secrets_client": self.secrets_client,
+                        "dbtools_client": self.dbtools_client, "dbtools_async_client": self.dbtools_async_client}
 
-        self.autonomous_database_id = None  # populated at runtime
-        self.wallet_secret_id = None        # populated at runtime
-        self.password_secret_id = None      # populated at runtime
-        self.connection_id = None           # populated at runtime
-        self.connection_string = None       # populated at runtime
+        self.password_secret_id = None  # populated at runtime
+        self.connection_id = None       # populated at runtime
 
         # When waiting for asynchronous calls, which status is considered "done"
-        self.terminal_states = ["FAILED", "SUCCEEDED", "CANCELED", "AVAILABLE"]
+        self.terminal_states = ["FAILED", "SUCCEEDED", "CANCELED"]
 
-    def create_db(self, db_workload="OLTP"):
-        """
-        Create an Autonomous Database with shared Exadata infrastructure and a public ip.
-        """
-        print("=== DBTools Example - Create Autonomous Database Named:", self.db_name)
-
-        response = self.db_client.list_autonomous_databases(
-            compartment_id=self.compartment_id,
-            display_name=self.db_name)
-
-        assert response, "list_autonomous_databases should return a response"
-
-        # If a database with same name already exists, abort the example to avoid
-        # accidentally deleting an existing Autonomous Database
-        assert len(response.data) == 0, f"a database with the name {self.db_name} should not already exist"
-
-        db_details = oci.database.models.CreateAutonomousDatabaseDetails(
-            admin_password=self.db_password,
-            compartment_id=self.compartment_id,
-            db_name=self.db_name,
-            display_name=self.db_name,
-            db_workload=db_workload,
-            whitelisted_ips=["0.0.0.0/0"],
-            cpu_core_count=1,
-            data_storage_size_in_tbs=1)
-
-        create_db_response = self.db_async_client.create_autonomous_database_and_wait_for_state(
-            create_autonomous_database_details=db_details,
-            wait_for_states=self.terminal_states)
-
-        assert create_db_response, "create_autonomous_database_and_wait_for_state should return a response"
-        assert create_db_response.data.id, "create_db_response data should contain an id"
-
-        self.autonomous_database_id = create_db_response.data.id
-        print(f"Created database. Name: {self.db_name}, ocid: {self.autonomous_database_id}")
-
-    def save_secrets_to_vault(self):
+    def save_dbpassword_to_vault(self):
         """
         Database Tools connections don't store secrets directly. Instead, they hold a pointer to
         secrets stored securely in a vault.
         """
-        print("=== DBTools Example - Save Secrets to Vault")
+        print("=== DBTools Example - Save DB Password to Vault")
 
-        db_wallet_secret_name = "dbtools-temp-secretw-" + datetime.utcnow().strftime("%m%d%H%M")
         db_password_secret_name = "dbtools-temp-secretp-" + datetime.utcnow().strftime("%m%d%H%M")
+        base64_secret_content = base64.b64encode(self.db_password.encode()).decode()
 
-        # The wallet zip file generated by the Autonomous Database contains the cwallet.sso file
-        # needed to establish an mTLS connection. Extract this and store it as a base64 encoded
-        # secret in the vault, along with a new secret for the database password.
+        self.password_secret_id = create_secret(oci_config=self.oci_config,
+                                                compartment_id=self.compartment_id,
+                                                vault_id=self.vault_id,
+                                                name=db_password_secret_name,
+                                                base64_secret_content=base64_secret_content,
+                                                clients=self.clients)[0]
 
-        response = self.db_client.get_autonomous_database(self.autonomous_database_id)
-        assert response, "get_autonomous_database should return a response"
-        assert response.data, "response should contain data"
-        autonomous_database = response.data
-
-        self.wallet_secret_id, self.password_secret_id = get_wallet_and_create_secrets(oci_config=self.oci_config,
-                                                                                       compartment_id=self.compartment_id,
-                                                                                       clients=self.clients,
-                                                                                       autonomous_database_id=self.autonomous_database_id,
-                                                                                       vault_id=self.vault_id,
-                                                                                       db_wallet_secret_name=db_wallet_secret_name,
-                                                                                       db_password_secret_name=db_password_secret_name,
-                                                                                       db_password=self.db_password)
-
-        # Get the connection string with mTLS from the database
-        assert autonomous_database.connection_strings, "autonomous_database should have connection strings"
-        assert autonomous_database.connection_strings.profiles, "connection_strings should have profiles"
-
-        profiles = autonomous_database.connection_strings.profiles
-        m_tls = oci.database.models.DatabaseConnectionStringProfile.TLS_AUTHENTICATION_MUTUAL
-        low = oci.database.models.DatabaseConnectionStringProfile.CONSUMER_GROUP_LOW
-        self.connection_string = next((p.value for p in profiles if
-                                       p.tls_authentication == m_tls and p.consumer_group == low), None)
-
-        print(f"Created secret. Name: {db_wallet_secret_name}, ocid: {self.wallet_secret_id}")
         print(f"Created secret. Name: {db_password_secret_name}, ocid: {self.password_secret_id}")
 
     def create_connection(self):
         """
         Given the configuration details provided above and the secrets stored in the vault,
-        create a Database Tools connection to the Autonomous Database.
+        create a Database Tools connection to the MySQL database.
         """
         print("=== DBTools Example - Create Connection Using:", self.connection_string)
 
-        # Setup SSO key store using wallet_secret_id (required for mTLS)
-        key_store = oci.database_tools.models.DatabaseToolsKeyStoreDetails(
-            key_store_type=oci.database_tools.models.DatabaseToolsKeyStoreDetails.KEY_STORE_TYPE_SSO,
-            key_store_content=oci.database_tools.models.DatabaseToolsKeyStoreContentSecretIdDetails(
-                value_type=oci.database_tools.models.DatabaseToolsKeyStoreContentSecretIdDetails.VALUE_TYPE_SECRETID,
-                secret_id=self.wallet_secret_id))
-
-        # ADMIN is automatically created when deploying a new Autonomous Database
-        user_name = "admin"
         user_password = oci.database_tools.models.DatabaseToolsUserPasswordSecretIdDetails(
             secret_id=self.password_secret_id)
 
         display_name = "conn_pysdk_" + datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
-        related_resource = oci.database_tools.models.CreateDatabaseToolsRelatedResourceDetails(
-            entity_type=oci.database_tools.models.CreateDatabaseToolsRelatedResourceDetails.ENTITY_TYPE_AUTONOMOUSDATABASE,
-            identifier=self.autonomous_database_id)
 
-        conn_details_for_create = oci.database_tools.models.CreateDatabaseToolsConnectionOracleDatabaseDetails(
+        conn_details_for_create = oci.database_tools.models.CreateDatabaseToolsConnectionMySqlDetails(
             compartment_id=self.compartment_id,
             connection_string=self.connection_string,
-            user_name=user_name,
+            user_name=self.db_username,
             user_password=user_password,
-            display_name=display_name,
-            related_resource=related_resource,
-            key_stores=[key_store])
+            display_name=display_name)
 
         response = self.dbtools_async_client.create_database_tools_connection_and_wait_for_state(
             conn_details_for_create, wait_for_states=self.terminal_states)
@@ -235,7 +166,7 @@ class DBToolsExample:
         connection = self.dbtools_client.get_database_tools_connection(self.connection_id)
         display_name = connection.data.display_name
 
-        connection_details_for_update = oci.database_tools.models.UpdateDatabaseToolsConnectionOracleDatabaseDetails(
+        connection_details_for_update = oci.database_tools.models.UpdateDatabaseToolsConnectionMySqlDetails(
             display_name=display_name + "_updated")
 
         response = self.dbtools_async_client.update_database_tools_connection_and_wait_for_state(self.connection_id,
@@ -259,7 +190,7 @@ class DBToolsExample:
         """
         print("=== DBTools Connection - Validate by id:", self.connection_id)
 
-        validate_detail = oci.database_tools.models.ValidateDatabaseToolsConnectionOracleDatabaseDetails()
+        validate_detail = oci.database_tools.models.ValidateDatabaseToolsConnectionMySqlDetails()
         response = self.dbtools_client.validate_database_tools_connection(self.connection_id, validate_detail)
         assert response, "validate_database_tools_connection should return a response"
         assert response.data, "response should contain data"
@@ -280,37 +211,20 @@ class DBToolsExample:
                                                                                                  wait_for_states=self.terminal_states)
         assert response, "delete_database_tools_connection_and_wait_for_state should return a response"
         assert response.data, "response should contain data"
+        assert response.data.status == "SUCCEEDED", f"delete connection should return status SUCCEEDED but was {response.data.status}"
 
-        status = response.data.status
-        assert status == "SUCCEEDED", f"delete connection should return status SUCCEEDED but was {status}"
-
-        print(f"Deleted connection. Status: {status}, ocid: {self.connection_id}.")
+        print(f"Deleted connection. ocid: {self.connection_id}.")
 
     def do_secrets_cleanup(self, days=2):
         """
         Schedule deletion of the secrets created by the example.
         """
         secret_deletion_time = datetime.now() + timedelta(days=days)
-        if self.password_secret_id is not None:
+        if self.password_secret_id:
             print("=== DBTools Example - Schedule Secret Deletion:", self.password_secret_id)
             delete_secret(clients=self.clients, secret_id=self.password_secret_id, deletion_time=secret_deletion_time)
         else:
             print("do_secrets_cleanup skipped (password)")
-
-        if self.wallet_secret_id is not None:
-            print("=== DBTools Example - Schedule Secret Deletion:", self.wallet_secret_id)
-            delete_secret(clients=self.clients, secret_id=self.wallet_secret_id, deletion_time=secret_deletion_time)
-        else:
-            print("do_secrets_cleanup skipped (wallet)")
-
-    def do_database_cleanup(self):
-        # Beware, this actually DELETES the specified DB
-        if self.autonomous_database_id is not None:
-            print("=== DBTools Example - Terminate Database:", self.autonomous_database_id)
-            self.db_client.delete_autonomous_database(self.autonomous_database_id)
-            print("Database terminated.")
-        else:
-            print("do_database_cleanup skipped")
 
 
 #
@@ -324,10 +238,9 @@ if __name__ == "__main__":
     example = None
 
     try:
-        example = DBToolsExample(compartment_id, vault_id, db_password)
+        example = DBToolsExample(compartment_id, vault_id, connection_string, db_username, db_password)
 
-        example.create_db()
-        example.save_secrets_to_vault()
+        example.save_dbpassword_to_vault()
         example.create_connection()
         example.get_connection()
         example.update_connection()
@@ -343,7 +256,6 @@ if __name__ == "__main__":
             print("Starting resource clean-up.")
             example.do_connection_cleanup()
             example.do_secrets_cleanup()
-            example.do_database_cleanup()
             print("Clean-up completed.")
         else:
             print("Skipping resource clean-up.")
--- examples/database_tools/readme.md
+++ examples/database_tools/readme.md
@@ -1,18 +1,20 @@
 # Database Tools Examples
 
 ## Introduction
-We show 3 Use Cases for the Database Tools Service
+We show 5 use cases for the Database Tools service
 
 ## Use Cases
-- ADB-S with Public IP
-- ADB-S with Private Endpoint
-- Bare Metal, VM and Exadata with Private IP
+- ADB-S with public ip
+- ADB-S with private endpoint
+- Bare Metal, VM and Exadata with private ip
+- MySQL with public ip
+- MySQL DB System with private endpoint
 
 ## ADB-S with Public IP
 ![](images/ADB-S.with.Public.IP.png)
 ### Example
 See: **adbs_create_connection_public_ip.py** where we:
-1. Create a new Autonomous Database
+1. Create a new Autonomous Database (ADB-S)
 2. Create required secrets
 3. Create a connection
 4. Validate the connection
@@ -21,16 +23,33 @@ See: **adbs_create_connection_public_ip.py** where we:
 ![](images/ADB-S.with.Private.Endpoint.png)
 ### Example
 See: **adbs_create_connection_with_pe.py** where we:
-1. Create a Database Tools Private Endpoint for a Reverse Connection to the Private Endpoint of an ADB-S
+1. Create a Database Tools private endpoint
 2. Create required secrets
-3. Create a connection
+3. Create a connection using the Database Tools private endpoint
 4. Validate the connection
 
 ## Bare Metal, DB System and Exadata with Private IP
 ![](images/Bare.Metal.VM.Exadata.with.Private.IP.png)
 ### Example
 See: **db_system_create_connection_private_subnet.py** where we:
-1. Create a Database Tools Private Endpoint for a Reverse Connection to a VM DB System
-2. Create required secrets
-3. Create a connection
+1. Create a Database Tools private endpoint
+2. Create required secret
+3. Create a connection using the Database Tools private endpoint
+4. Validate the connection
+
+## MySQL with Public IP
+![](images/MySQL.with.Public.IP.png)
+### Example
+See: **mysql_create_connection_public_ip.py** where we:
+1. Create required secret
+2. Create a connection
+3. Validate the connection
+
+## MySQL DB System with Private Edpoint
+![](images/MySQL.DB.System.with.PE.png)
+### Example
+See: **mysql_db_system_create_connection_with_pe.py** where we:
+1. Create a Database Tools private endpoint
+2. Create required secret
+3. Create a connection using the Database Tools private endpoint
 4. Validate the connection
